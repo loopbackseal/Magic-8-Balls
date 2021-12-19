cb621585d2019746912adb7cc0d97e4d4820ca72
loopbackseal <young961027@gmail.com>
2021-12-19 23:17:03 +0900

    Initial Setup

---
+++ Magic-8-Balls/Magic-8-Balls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic-8-Balls
+//
+//  Created by Young Soo Hwang on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
