33f12cb2e68b58c567b26dfd3d6773496ea73d96
loopbackseal <young961027@gmail.com>
2021-12-20 15:11:18 +0900

    Finish Magic-8-Balls without AutoLayout

--- Magic-8-Balls/Magic-8-Balls/ViewController.swift
+++ Magic-8-Balls/Magic-8-Balls/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let imageArray = [UIImage(named: "ball1"),
+            UIImage(named: "ball2"),
+            UIImage(named: "ball3"),
+            UIImage(named: "ball4"),
+            UIImage(named: "ball5"),]
+        imageView.image = imageArray[Int.random(in: 0...4)]
+    }
 }
 
